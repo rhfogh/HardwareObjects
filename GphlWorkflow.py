db9dbc806a73145c2a7dcfb3647c579279cad62c
rhfogh <rhfogh@globalphasing.com>
2017-04-06 14:26:36 +0100

    Added new, empty Workflow HO

---
+++ GphlWorkflow.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python
+# encoding: utf-8
+"""Global phasing workflow runner
+"""
+
+__copyright__ = """
+  * Copyright © 2016 - 2017 by Global Phasing Ltd.
+"""
+__author__ = "rhfogh"
+__date__ = "06/04/17"
+
+
